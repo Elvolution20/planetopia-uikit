50bffba1540aa4ef24f9f82a88a259b050c96606
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2023-02-22 14:00:31 +0100

    commit

--- src/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-import { PantherTheme } from "./theme";
-
-declare module "styled-components" {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends PantherTheme {}
-}
